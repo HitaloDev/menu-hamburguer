fed58ebb0c6baaee9053b5d5f11bb9bf2272a8f5
hitalodev <hitalohpa@gmail.com>
2023-11-06 17:56:33 -0300

    Ajustes de interface

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,12 @@
 const hamburguer = document.getElementById('hamburguer');
 let listaUl = document.getElementById('lista-ul');
 
+
+
 hamburguer.addEventListener('click', () => {
+        
         listaUl.classList.toggle('active');
+        hamburguer.classList.toggle('rotated');
 
         if(listaUl.classList.contains('active')){
             hamburguer.src="./assets/menu 2.png"
