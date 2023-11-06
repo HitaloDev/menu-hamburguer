056e65b298d8e92cc0e27f836653b7adbee990dd
hitalodev <hitalohpa@gmail.com>
2023-11-06 17:28:14 -0300

    criação do projeto

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+const hamburguer = document.getElementById('hamburguer');
+let listaUl = document.getElementById('lista-ul');
+
+hamburguer.addEventListener('click', () => {
+        listaUl.classList.toggle('active');
+
+        if(listaUl.classList.contains('active')){
+            hamburguer.src="./assets/menu 2.png"
+        }else {
+            hamburguer.src="./assets/menu.png"
+        }
+
+});
